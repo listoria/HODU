449fd8f0010deab889734d45d0520ee24a3539f3
Geun Ah KIM <geunah1109@gmail.com>
2024-08-28 18:36:43 +0900

    map address

--- script.js
+++ script.js
@@ -8,6 +8,35 @@ var mapContainer = document.getElementById("map"), // 지도를 표시할 div
 // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
+var geocoder = new kakao.maps.services.Geocoder();
+
+// 주소로 좌표를 검색합니다
+geocoder.addressSearch(
+  "제주특별자치도 제주시 첨단로 330",
+  function (result, status) {
+    // 정상적으로 검색이 완료됐으면
+    if (status === kakao.maps.services.Status.OK) {
+      var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+      // 결과값으로 받은 위치를 마커로 표시합니다
+      var marker = new kakao.maps.Marker({
+        map: map,
+        position: coords,
+      });
+
+      // 인포윈도우로 장소에 대한 설명을 표시합니다
+      var infowindow = new kakao.maps.InfoWindow({
+        content:
+          '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>',
+      });
+      infowindow.open(map, marker);
+
+      // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+      map.setCenter(coords);
+    }
+  }
+);    
+
 // 스크롤 탑 버튼
 
 window.onscroll = function () {

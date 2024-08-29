1fb72254cbec0e3e8c43e94bbdac88deefda9ba6
Geun Ah KIM <geunah1109@gmail.com>
2024-08-29 10:07:13 +0900

    change map

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 ﻿// 지도
-var mapContainer = document.getElementById('map'),
-    mapOption = { 
-        center: new kakao.maps.LatLng(33.450701, 126.570667),
-        level: 3
-    };
+var mapContainer = document.getElementById("map"),
+  mapOption = {
+    center: new kakao.maps.LatLng(33.44235462471831, 126.57144169440214),
+    level: 3,
+  };
 
 var map = new kakao.maps.Map(mapContainer, mapOption); 
 
